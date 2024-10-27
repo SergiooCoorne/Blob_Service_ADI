759ed3597121626960c0f3516c7b23ce2ffcf5da
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2024-10-27 12:47:37 +0100

    Añadidas nuevas funcionalidades de la capa de presentacion y del cliente de pruebas

--- client_blob.py
+++ client_blob.py
@@ -7,6 +7,8 @@ import requests
 def _assert_status_ok_(status_code):
     if status_code not in [200, 204]:
         raise RuntimeError('Unknown response from API')
+    else:
+        return True
     
 class Client:
     def __init__(self, root_api: str) -> None:
@@ -25,7 +27,52 @@ class Client:
         _assert_status_ok_(response.status_code)
         print(f'Status Code: {response.status_code}')
     
+    def delet_blob(self, blob_id: str) -> None:
+        response = requests.delete(
+            f'{self.root_api}/blob/{blob_id}',
+            headers = {'AuthToken': '1234'}
+        )
+        if _assert_status_ok_(response.status_code):
+            print(f'Blob_ID removed: "{blob_id}" With status Code: {response.status_code}')
+    
+    def get_blob(self, blob_id: str) -> None:
+        response = requests.get(
+            f'{self.root_api}/blob/{blob_id}/data',
+            headers = {'AuthToken': '1234'}
+        )
+        _assert_status_ok_(response.status_code)
+            
+    def post_patch_blob(self, blob_id: str, data: str, request: str) -> None:
+        if request == 'POST':
+            response = requests.post(
+                f'{self.root_api}/blob/{blob_id}/data',
+                headers = {'AuthToken': '1234'},
+                json = {
+                    'multipart': data
+                }
+            )
+            _assert_status_ok_(response.status_code)
+            print(f'Status Code: {response.status_code}')
+        
+        elif request == 'PATCH':
+            response = requests.patch(
+                f'{self.root_api}/blob/{blob_id}/data',
+                headers = {'AuthToken': '1234'},
+                json = {
+                    'multipart': data
+                }
+            )
+            _assert_status_ok_(response.status_code)
+            print(f'Status Code: {response.status_code}')
+            
+        else:
+            raise ValueError('Unknown request')
+    
 if __name__ == '__main__':
     client = Client('http://localhost:1234/api/v1')
-    writted_by = ['Luis', 'Paco', 'Antonio']
-    client.put_blob('1234', 'Sergio', writted_by, b'1234')
+    # writted_by = ['Luis', 'Paco', 'Antonio']
+    # client.put_blob('1234', 'Sergio', writted_by, b'1234')
+    #client.delet_blob('1234567889')
+    client.get_blob('1234567889')
+    client.post_patch_blob('1234567889', '1234', 'POST')
+    client.post_patch_blob('4564651534', '5678', 'PATCH')
--- presentacion_blob.py
+++ presentacion_blob.py
@@ -12,6 +12,7 @@ args = parser.parse_args()
 
 ROOT_API = '/api/v1'
 
+'''Recurso de la API correspondiente con "API_ROOT/blob" '''
 @app.route(f'{ROOT_API}/blob', methods = ('PUT',))
 def add_blob():
     auth_token = request.headers.get('AuthToken')
@@ -35,9 +36,37 @@ def add_blob():
     #     print(f'Writer: {i}')
     
     #AQUI IRIA LA LLAMADA A LA CAPA DE NEGOCIO PARA AVERIGUAR SI LA OPERACION SE PUEDE REALIZAR O NO
-    
     return Response(f'Created. Data: ', status=201)
 
+'''Recurso de la API correspondiente con "API_ROOT/blob/{blob_id}" '''
+@app.route(f'{ROOT_API}/blob/<blob_id>', methods = ('DELETE',))
+def delete_blob(blob_id):
+    auth_token = request.headers.get('AuthToken')
+    
+    if not auth_token:
+        return Response('Unauthorized', status = 401)
+    
+    
+    #A PARTIR DE AQUI HABRA QUE LLAMAR A LA COPA DE NEGOCIO PARA QUE VERIFIQUE SI EL AUTH_TOKEN ES VALIDO Y SI EL BLOB_ID EXISTE
+    return Response(f'Deleted {blob_id}', status = 204)
+    
+'''Recurso de la API correspondiente con "API_ROOT/blob/{blob_id}/data" '''
+@app.route(f'{ROOT_API}/blob/<blob_id>/data', methods = ('GET','POST','PATCH'))
+def data_blob(blob_id):
+    auth_token = request.headers.get('AuthToken')
+    
+    if not auth_token:
+        return Response('Unauthorized', status = 401)
+    
+    if request.method == 'GET':
+        #A PARTIR DE AQUI HABRA QUE LLAMAR A LA CAPA DE NEGOCIO PARA QUE DEVUELVA LOS DATOS DEL BLOB_ID O NO
+        return Response('', status = 200)
+    
+    if request.method == 'POST' or request.method == 'PATCH':
+        multipart = request.json.get('multipart')
+        #A PARTIR DE AQUI HABRA QUE LLAMAR A LA CAPA DE NEGOCIO PARA QUE SUSTITUYA LOS DATOS DEL BLOB_ID O NO
+        return Response('', status = 200)
+    
 
 if __name__ == '__main__':
     app.run(host = f'{args.listening}', port =args.port)
