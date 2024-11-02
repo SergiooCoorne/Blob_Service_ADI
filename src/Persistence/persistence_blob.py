af2ef0cad8f441a4fa4546114ab7f5f91ffed443
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2024-11-02 10:59:08 +0100

    Capa de persistencia junto a sus test con cobertura del 100%

---
+++ src/Persistence/persistence_blob.py
@@ -0,0 +1,130 @@
+import secrets
+import json
+import base64
+import os
+from typing import Union
+
+
+class BlobNotFound(Exception):
+    """Excepción para cuando un Blob_ID no se encuentra en el JSON."""
+    def __init__(self, blob_id):
+        super().__init__(f"Blob con ID '{blob_id}' no encontrado.")
+        self.blob_id = blob_id
+
+class Blob:
+    def __init__(self, name: str, roles: list[str], data: bytes):
+        self.name = name
+        self.blob_id = secrets.token_hex(8)
+        self.roles = roles
+        self.data = data
+    
+    def get_blob_id(self) -> str:
+        return self.blob_id
+    
+    def get_blob_name(self) -> str:
+        return self.name
+    
+    def get_blob_data(self) -> bytes:
+        return self.data
+    
+    def get_blob_roles(self) -> list:
+        return self.roles
+
+class Persistence:
+    def __init__(self, root_persistence: str):
+        self.root_persistence = root_persistence
+        if not os.path.exists(self.root_persistence):
+            with open(self.root_persistence, 'w') as f:
+                json.dump({}, f)  # Escribir un objeto JSON vacío
+
+    def create_blob(self, name: str, roles: list[str], data: bytes) -> Union[str, bool]:
+        rtn = False #Valor que se va a devolver para verificar si se ha hecho de forma correcta la operacion o no
+        new_blob = Blob(name, roles, data)
+        data_encoded = base64.b64encode(new_blob.get_blob_data()).decode('utf-8')
+    
+        #Leemos los datos del JSON
+        with open(self.root_persistence, 'r',) as persistence_json:
+            data_json = json.load(persistence_json)
+    
+        # Agreamos ahora el nuevo Blob cuya clave sera su Blob_ID, y de esta manera lo podemos identificar en el JSON
+        blob_data = {
+            "Blob_Name": new_blob.get_blob_name(),
+            "Blob_Roles": new_blob.get_blob_roles(),
+            "Blob_Data": data_encoded
+        }
+        data_json[new_blob.get_blob_id()] = blob_data
+
+       
+        with open(self.root_persistence, 'w', encoding='utf-8') as persistence_json:
+            json.dump(data_json, persistence_json, indent=4)
+            rtn = True
+
+        return new_blob.get_blob_id(), rtn
+
+    def get_blob_data(self, blob_id: str) -> bytes:
+        #Leemos los datos del JSON
+        with open(self.root_persistence, 'r') as persistence_json:
+            data_json = json.load(persistence_json)
+        
+        #Comprobamos que el Blob_ID que nos estan pasando esta dentro del archivo JSON
+        if not blob_id in data_json:
+            raise BlobNotFound(blob_id)
+        
+        data = base64.b64decode(data_json[blob_id]['Blob_Data'])
+        #print(f'Data del blob ID: {blob_id}: {data}')
+
+        return data
+    
+    def delete_blob(self, blob_id: str) -> bool:
+        rtn = False #Valor que se va a devolver para verificar si se ha hecho de forma correcta la operacion o no
+
+        #Leemos los datos del JSON
+        with open(self.root_persistence, 'r') as persistence_json:
+            data_json = json.load(persistence_json)
+
+        #Comprobamos que el Blob_ID esta dentro del JSON
+        if not blob_id in data_json:
+            raise BlobNotFound(blob_id)
+        
+        #Borramos el blob_ID del JSON
+        del data_json[blob_id]
+
+        #Escribimos del nuevo el JSON
+        with open(self.root_persistence, 'w') as persistence_json:
+            json.dump(data_json, persistence_json, indent = 4)
+            rtn = True
+
+        return rtn
+    
+    def modify_blob(self, blob_id: str, new_name: str, new_roles: list[str], new_data: bytes) -> bool:
+        rtr = False #Valor que se va a devolver para verificar si se ha hecho de forma correcta la operacion o no
+
+        #Leeemos el JSON para obtener los datos que hay en el
+        with open(self.root_persistence, 'r') as persistence_json:
+            data_json = json.load(persistence_json)
+
+        #Comprobamos que el Blob_ID esta dentro del JSON
+        if not blob_id in data_json:
+            raise BlobNotFound(blob_id)
+        
+        #Procedemos a modificar lo que se quiere modificar el Blob
+        data_json[blob_id]["Blob_Name"] = new_name
+        data_json[blob_id]["Blob_Roles"] = new_roles
+        data_json[blob_id]["Blob_Data"] = base64.b64encode(new_data).decode('utf-8')
+
+        #Escribimos del nuevo el JSON
+        with open(self.root_persistence, 'w') as persistence_json:
+            json.dump(data_json, persistence_json, indent = 4)
+            rtr = True
+
+        return rtr
+
+# if __name__ == '__main__':
+#     persistencia = Persistence('./persistence.json')
+#     blob_id_1 = persistencia.create_blob('Blob_1', ['admin', 'user'], data = 'Hola'.encode())
+#     blob_id_2 = persistencia.create_blob('Blob_2', ['admin', 'user'], data = 'Adios'.encode())
+#     print(persistencia.get_blob_data(blob_id_1))
+#     persistencia.delete_blob(blob_id_2)
+#     persistencia.modify_blob(blob_id_1, 'Blob_1_1', ['admin_1', 'user_2'], new_data = 'Adios'.encode())
+#     print(persistencia.get_blob_data(blob_id_1))
+
---
+++ test/test_persistence.py
@@ -0,0 +1,73 @@
+from src.Persistence.persistence_blob import Blob, Persistence, BlobNotFound
+import pytest
+import os
+
+@pytest.fixture
+def blob_created():
+    blob_created = Blob('Test_1', ['rol_test1', 'rol_test_2'], 'Test_Create_blob'.encode())
+    yield blob_created
+
+def test_blob_1(blob_created):
+    instance_blob = blob_created
+    assert instance_blob.get_blob_id is not None
+    assert instance_blob.get_blob_name is not None
+    assert instance_blob.get_blob_roles is not None
+    assert instance_blob.get_blob_data is not None    
+
+@pytest.fixture
+def blob():
+    obj = Persistence('./test/json_test.json')
+    blob_id, rtr = obj.create_blob('Test_1', ['rol_test1', 'rol_test_2'], 'Test_Create_blob'.encode())
+    yield obj, blob_id, rtr
+
+@pytest.fixture(scope='session', autouse = True)
+def cleanup_json_file():
+    yield
+    if os.path.exists('./test/json_test.json'):
+        os.remove('./test/json_test.json')
+
+def test_create_blob(blob):
+    obj, blob_id, rtr = blob
+    assert blob_id is not None
+    assert rtr is True
+
+def test_1_get_data(blob):
+    obj, blob_id, rtr = blob
+
+    data = obj.get_blob_data(blob_id)
+    assert data is not None
+
+def test_2_get_data(blob):
+    obj, blob_id, rtr = blob
+
+    blob_id = 'dbjqkbdiq'
+    with pytest.raises(BlobNotFound, match=f"Blob con ID '{blob_id}' no encontrado"):
+        obj.get_blob_data(blob_id)
+
+def test_delete_blob(blob):
+    obj, blob_id, rtr = blob
+
+    rtr = obj.delete_blob(blob_id)
+
+    assert rtr is True
+
+def test_2_detele_blob(blob):
+    obj, blob_id, rtr = blob
+
+    blob_id = 'dbjqkbdiq'
+    with pytest.raises(BlobNotFound, match=f"Blob con ID '{blob_id}' no encontrado"):
+        obj.delete_blob(blob_id)
+
+def test_1_modify_blob(blob):
+    obj, blob_id, rtr = blob
+
+    rtr = obj.modify_blob(blob_id, 'Blob_Test_Modified', ['rol_test1_modified', 'rol_test2_modified'], new_data = 'Test_Create_blob_Modified'.encode())
+    assert rtr is True
+
+def test_2_modify_blob(blob):
+    obj, blob_id, rtr = blob
+
+    blob_id = 'dbjqkbdiq'
+
+    with pytest.raises(BlobNotFound, match=f"Blob con ID '{blob_id}' no encontrado"):
+       obj.modify_blob(blob_id, 'Blob_Test_Modified', ['rol_test1_modified', 'rol_test2_modified'], new_data = 'Test_Create_blob_Modified'.encode())
